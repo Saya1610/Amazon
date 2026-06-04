8607261f8fc7139c3755a8e721495fdd96fbe3fe
Saya1610 <sayalijadhav1610@gmail.com>
2026-06-04 16:04:44 +0530

    added commited mesaage

--- Amezon_Automation/src/test/java/pom/CareersPage.java
+++ Amezon_Automation/src/test/java/pom/CareersPage.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CareersPage {
-
+//careersPage
 	@FindBy (xpath = "//a[text()='View open jobs']")
 	private List<WebElement> viewOpenJobsBtn ; 
 	
